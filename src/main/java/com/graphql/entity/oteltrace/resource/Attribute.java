bf60ab44b607b5add9dd397ed6047b9ef079be68
durga1808 <durgalakshmi3713@gmail.com>
2023-11-09 15:38:22 +0530

    graphql using to get the data

--- src/main/java/com/graphql/entity/oteltrace/resource/Attribute.java
+++ src/main/java/com/graphql/entity/oteltrace/resource/Attribute.java
@@ -1,32 +1,18 @@
-// package com.graphql.entity.oteltrace.resource;
+// // package com.graphql.entity.oteltrace.resource;
 
-<<<<<<< HEAD
-import com.graphql.entity.oteltrace.resource.attributes.Value;
-=======
-// import org.eclipse.microprofile.graphql.Name;
+// // import org.eclipse.microprofile.graphql.Name;
 
-// import com.graphql.entity.oteltrace.resource.attributes.Value;
->>>>>>> 879bfc1c8b1c51feabf873a749f1ad62ed1ce4f4
+// // import com.graphql.entity.oteltrace.resource.attributes.Value;
 
-// import lombok.AllArgsConstructor;
-// import lombok.Data;
-// import lombok.NoArgsConstructor;
+// // import lombok.AllArgsConstructor;
+// // import lombok.Data;
+// // import lombok.NoArgsConstructor;
 
-<<<<<<< HEAD
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class Attribute {
-    private String key;
-    private Value value;
-}
-=======
 // @Data
 // @AllArgsConstructor
 // @NoArgsConstructor
-// @Name("AttributeLog")
 // public class Attribute {
 //     private String key;
 //     private Value value;
 // }
->>>>>>> 879bfc1c8b1c51feabf873a749f1ad62ed1ce4f4
+
